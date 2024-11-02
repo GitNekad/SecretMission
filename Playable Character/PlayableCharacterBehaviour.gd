d44fc1562082281b8865eec4155e241e73a20787
Eloi <48120733+GitNekad@users.noreply.github.com>
2024-11-02 20:25:13 +0100

    Added first version of playable character behaviour and main scene for the game

---
+++ Playable Character/PlayableCharacterBehaviour.gd
@@ -0,0 +1,4 @@
+extends CharacterBody2D
+class_name PlayableCharacterBehaviour
+
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
---
+++ Playable Character/PlayerBehaviour.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var character : PlayableCharacterBehaviour
+var characterVelocity : Vector2
+var characterLookingDirection : Vector2 = Vector2(0,1)
+var _characterPack = preload("res://Playable Character/PlayableCharacter.tscn")
+
+var CHARACTER_SPEED = 300
+
+func _ready():
+	instantiateCharacter(Vector2i(-263,152))
+
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	characterVelocity = input_dir * CHARACTER_SPEED
+	animateSprite()
+
+func _physics_process(delta):
+	get_input()
+	character.move_and_collide(characterVelocity * delta)
+
+func instantiateCharacter(position: Vector2i):
+	character = _characterPack.instantiate() 
+	self.add_child(character)
+	character.position = position
+
+func animateSprite():
+	var lookingAt : String
+	var movement : String
+	if characterVelocity.length() < 0.001:
+		movement = "Idle"
+	else:
+		movement = "Walk"
+		characterLookingDirection = characterVelocity
+	character.sprite.flip_h = !characterLookingDirection.x > 0
+	if absf(characterLookingDirection.x) > absf(characterLookingDirection.y):
+		lookingAt = "S-"
+	elif characterLookingDirection.y > 0:
+		lookingAt = "F-"
+	else:
+		lookingAt = "B-"
+	if character.sprite.animation != lookingAt+movement:
+		character.sprite.play(lookingAt+movement)
