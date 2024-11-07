97b1990a15c87721b805776a682abd14fab14f67
Eloi <48120733+GitNekad@users.noreply.github.com>
2024-11-07 08:26:42 +0100

    Added support for up to 8 controllers

--- Playable Character/PlayerBehaviour.gd
+++ Playable Character/PlayerBehaviour.gd
@@ -1,21 +1,42 @@
 extends Node
 
+@export var player : int
+@export var spawnPosition : Vector2i
+
 var character : PlayableCharacterBehaviour
 var characterVelocity : Vector2
 var characterLookingDirection : Vector2 = Vector2(0,1)
 var _characterPack = preload("res://Playable Character/PlayableCharacter.tscn")
 
 var CHARACTER_SPEED = 300
+var _playerControlsAffix : String
 
 func _ready():
-	instantiateCharacter(Vector2i(-263,152))
+	instantiateCharacter(spawnPosition)
+	match player:
+		0:
+			_playerControlsAffix = "P0"
+		1:
+			_playerControlsAffix = "P1"
+		2:
+			_playerControlsAffix = "P2"
+		3:
+			_playerControlsAffix = "P3"
+		4:
+			_playerControlsAffix = "P4"
+		5:
+			_playerControlsAffix = "P5"
+		6:
+			_playerControlsAffix = "P6"
+		7:
+			_playerControlsAffix = "P7"
 
 func _input(event):
-	if event.is_action_pressed("interact_P0"):
+	if event.is_action_pressed("interact_"+_playerControlsAffix):
 		interact()
 
 func get_input():
-	var input_dir = Input.get_vector("walk_left_P0", "walk_right_P0", "walk_up_P0", "walk_down_P0")
+	var input_dir = Input.get_vector("walk_left_"+_playerControlsAffix, "walk_right_"+_playerControlsAffix, "walk_up_"+_playerControlsAffix, "walk_down_"+_playerControlsAffix)
 	characterVelocity = input_dir * CHARACTER_SPEED
 	animateSprite()
 
